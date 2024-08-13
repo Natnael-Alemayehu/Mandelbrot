62be302fb3e5fa771881faf2be4792847cfc66b9
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-08-13 18:20:24 +0300

    Initial commit for advanced mandelbrot

--- main.go
+++ main.go
@@ -4,62 +4,140 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"math/cmplx"
 	"os"
 )
 
+type Func func(complex128) complex128
+
+var colorPool = []color.RGBA{
+	{170, 57, 57, 255},
+	{170, 108, 57, 255},
+	{34, 102, 102, 255},
+	{45, 136, 45, 255},
+}
+
+var chosenColors = map[complex128]color.RGBA{}
+
 func main() {
 	const (
-		xmin, ymin, xmax, ymax = -1.9, -1.9, +1.9, +1.9
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
-		epsX                   = (xmax - xmin) / width
-		epsY                   = (ymax - ymin) / height
 	)
-	offX := []float64{-epsX, epsX}
-	offY := []float64{-epsY, epsY}
-	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
-			// Super sampling
-			subPixels := make([]color.Color, 0)
-			for i := 0; i < 2; i++ {
-				for j := 0; j < 2; j++ {
-					z := complex(x+offX[i], y+offY[j])
-					subPixels = append(subPixels, mandelbrot(z))
-				}
-			}
 			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, z4(z))
 		}
 	}
-	png.Encode(os.Stdout, img)
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
 }
 
 func mandelbrot(z complex128) color.Color {
-	const iterations = 255
-	const contrast = 17
+	const iterations = 200
+	const contrast = 15
 
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
-		if cmplx.Abs(v) > 2.5 {
-			return color.Gray{255 - contrast*n}
+		if cmplx.Abs(v) > 2 {
+			switch {
+			case n > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(n)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
 		}
 	}
-	return color.RGBA{12, 12, 44, 255}
+	return color.Black
 }
 
-func avg(colors []color.Color) color.Color {
-	var r, g, b, a uint16
-	n := len(colors)
-	for _, c := range colors {
-		r_, g_, b_, a_ := c.RGBA()
-		r += uint16(r_ / uint32(n))
-		g += uint16(g_ / uint32(n))
-		b += uint16(b_ / uint32(n))
-		a += uint16(a_ / uint32(n))
+// Some other interesting functions:
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//
+//	= z - (z^4 - 1) / (4 * z^3)
+//	= z - (z - 1/z^3) / 4
+func z4(z complex128) color.Color {
+	f := func(z complex128) complex128 {
+		return z*z*z*z - 1
 	}
-	return color.RGBA64{r, g, b, a}
+	fPrime := func(z complex128) complex128 {
+		return (z - 1/(z*z*z)) / 4
+	}
+	return newton(z, f, fPrime)
+}
+
+// f(x) = x^4 + 2x^3 + 3x^2 + 4x - 5
+//
+// z' = z - f(z)/f'(z)
+//
+//	= z - (z^4 + 2z^3 + 3z^2 + 4z - 5) / (4z^3 + 6z^2 + 6z + 4)
+func quartic(z complex128) color.Color {
+	f := func(z complex128) complex128 {
+		return z*z*z*z + 2*z*z*z + 3*z*z + 4*z - 5
+	}
+	fPrime := func(z complex128) complex128 {
+		return (z*z*z*z + 2*z*z*z + 3*z*z + 4*z - 5) / (4*z*z*z + 6*z*z + 6*z + 4)
+	}
+	return newton(z, f, fPrime)
+}
+
+func newton(z complex128, f Func, fPrime Func) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= fPrime(z)
+		if cmplx.Abs(f(z)) < 1e-6 {
+			root := complex(round(real(z), 4), round(imag(z), 4))
+			c, ok := chosenColors[root]
+			if !ok {
+				if len(colorPool) == 0 {
+					panic("no colors left")
+				}
+				c = colorPool[0]
+				colorPool = colorPool[1:]
+				chosenColors[root] = c
+			}
+			// Convert to YCbCr to make producing different shades easier.
+			y, cb, cr := color.RGBToYCbCr(c.R, c.G, c.B)
+			scale := math.Log(float64(i)) / math.Log(iterations)
+			y -= uint8(float64(y) * scale)
+			return color.YCbCr{y, cb, cr}
+		}
+	}
+	return color.Black
+}
+
+func round(f float64, digits int) float64 {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	pow := math.Pow10(digits)
+	return math.Trunc(f*pow+math.Copysign(0.5, f)) / pow
 }
